5cd16fdf6bbacb7ffdb6110a464e7a28270ac3cd
Alfeu Neto <0001152786@senaimgaluno.com.br>
2025-12-17 22:33:51 -0300

    Inserção dos scripts do jogador e do inimigo

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float velocidade = 2f;
+    public Transform pontoEsquerda;
+    public Transform pontoDireita;
+
+    Rigidbody2D rb;
+    bool indoDireita = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (indoDireita)
+        {
+            rb.linearVelocity = new Vector2(velocidade, rb.linearVelocity.y);
+
+            if (transform.position.x >= pontoDireita.position.x)
+                indoDireita = false;
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(-velocidade, rb.linearVelocity.y);
+
+            if (transform.position.x <= pontoEsquerda.position.x)
+                indoDireita = true;
+        }
+    }
+
+
+    public void Morrer()
+    {
+        Destroy(gameObject);
+    }
+}
